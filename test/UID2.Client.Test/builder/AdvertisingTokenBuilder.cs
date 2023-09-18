adbc3e3e9f310fb1620b020149cd409bdddf5062
Aleksandrs Ulme <aleksandrs.ulme@thetradedesk.com>
2023-09-18 17:49:41 +0800

    Addressed further feedback

--- test/UID2.Client.Test/builder/AdvertisingTokenBuilder.cs
+++ test/UID2.Client.Test/builder/AdvertisingTokenBuilder.cs
@@ -7,79 +7,65 @@ namespace uid2_client.test.builder
 {
     internal class AdvertisingTokenBuilder
     {
-        private TokenVersion _tokenVersion = TokenVersion.V4;
-        private string _rawUid = EXAMPLE_UID;
-        private Key _masterKey = MASTER_KEY;
-        private Key _siteKey = SITE_KEY;
-        private readonly int _siteId = SITE_ID;
-        private int _privacyBits = PrivacyBitsBuilder.Builder().WithAllFlagsDisabled().Build();
-        private DateTime _expiry = DateTime.UtcNow.AddHours(1);
-        private readonly IdentityScope _identityScope = IdentityScope.UID2;
-        private Func<string, Key, int, Key, UID2TokenGenerator.Params, string> _generateUid2TokenV2;
-        private Func<string, Key, int, Key, UID2TokenGenerator.Params, string> _generateUid2TokenV3;
-        private Func<string, Key, int, Key, UID2TokenGenerator.Params, string> _generateUid2TokenV4;
-
-        public AdvertisingTokenBuilder(
-            Func<string, Key, int, Key, UID2TokenGenerator.Params, string> generateUid2TokenV2,
-            Func<string, Key, int, Key, UID2TokenGenerator.Params, string> generateUid2TokenV3,
-            Func<string, Key, int, Key, UID2TokenGenerator.Params, string> generateUid2TokenV4
-        )
-        {
-            _generateUid2TokenV2 = generateUid2TokenV2;
-            _generateUid2TokenV3 = generateUid2TokenV3;
-            _generateUid2TokenV4 = generateUid2TokenV4;
-        }
+        public TokenVersion Version { get; private set; } = TokenVersion.V4;
+        public string RawUid { get; private set; } = EXAMPLE_UID;
+        public Key MasterKey { get; private set; } = MASTER_KEY;
+        public Key SiteKey { get; private set; } = SITE_KEY;
+        public int SiteId => SITE_ID;
+        public int PrivacyBits { get; private set; } = PrivacyBitsBuilder.Builder().WithAllFlagsDisabled().Build();
+        public DateTime Expiry { get; private set; } = DateTime.UtcNow.AddHours(1);
+        public IdentityScope Scope => IdentityScope.UID2;
 
         internal static AdvertisingTokenBuilder Builder()
         {
-            return new AdvertisingTokenBuilder(UID2TokenGenerator.GenerateUid2TokenV2, UID2TokenGenerator.GenerateUid2TokenV3, UID2TokenGenerator.GenerateUid2TokenV4);
+            return new AdvertisingTokenBuilder();
         }
 
         internal AdvertisingTokenBuilder WithVersion(TokenVersion version)
         {
-            _tokenVersion = version;
+            this.Version = version;
             return this;
         }
 
         internal AdvertisingTokenBuilder WithRawUid(string rawUid)
         {
-            _rawUid = rawUid;
+            RawUid = rawUid;
             return this;
         }
 
         internal AdvertisingTokenBuilder WithMasterKey(Key masterKey)
         {
-            _masterKey = masterKey;
+            MasterKey = masterKey;
             return this;
         }
 
         internal AdvertisingTokenBuilder WithSiteKey(Key siteKey)
         {
-            _siteKey = siteKey;
+            SiteKey = siteKey;
             return this;
         }
 
         internal AdvertisingTokenBuilder WithPrivacyBits(int privacyBits)
         {
-            _privacyBits = privacyBits;
+            PrivacyBits = privacyBits;
             return this;
         }
 
         internal AdvertisingTokenBuilder WithExpiry(DateTime expiry)
         {
-            _expiry = expiry;
+            Expiry = expiry;
             return this;
         }
 
         internal string Build()
         {
-            var encryptParams = new UID2TokenGenerator.Params().WithPrivacyBits(_privacyBits).WithTokenExpiry(_expiry);
-            encryptParams.IdentityScope = (int)_identityScope;
-            return _tokenVersion switch
+            var encryptParams = new UID2TokenGenerator.Params().WithPrivacyBits(PrivacyBits).WithTokenExpiry(Expiry);
+            encryptParams.IdentityScope = (int) Scope;
+            return Version switch
             {
-                TokenVersion.V2 => _generateUid2TokenV2(_rawUid, _masterKey, _siteId, _siteKey, encryptParams),
-                TokenVersion.V3 => _generateUid2TokenV3(_rawUid, _masterKey, _siteId, _siteKey, encryptParams),
-                TokenVersion.V4 => _generateUid2TokenV4(_rawUid, _masterKey, _siteId, _siteKey, encryptParams),
+                TokenVersion.V2 => UID2TokenGenerator.GenerateUid2TokenV2(RawUid, MasterKey, SiteId, SiteKey, encryptParams),
+                TokenVersion.V3 => UID2TokenGenerator.GenerateUid2TokenV3(RawUid, MasterKey, SiteId, SiteKey, encryptParams),
+                TokenVersion.V4 => UID2TokenGenerator.GenerateUid2TokenV4(RawUid, MasterKey, SiteId, SiteKey, encryptParams),
                 _ => throw new ArgumentOutOfRangeException()
             };
         }
--- test/UID2.Client.Test/builder/AdvertisingTokenBuilderTest.cs
+++ test/UID2.Client.Test/builder/AdvertisingTokenBuilderTest.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using UID2.Client;
-using UID2.Client.Utils;
 using Xunit;
 using static uid2_client.test.builder.AdvertisingTokenBuilder;
 using static uid2_client.test.TestData;
@@ -44,51 +43,70 @@ namespace uid2_client.test.builder
             }
         }
         
-        [Fact]
-        public void CreatesTokens()
+        public class BuilderSetterTests
         {
-            var privacyBits = 20;
-            var expiry = DateTime.Today;
-            var masterKey = new Key(MASTER_KEY_ID, -1, DateTime.Now, DateTime.Now, DateTime.Now, MASTER_SECRET);
-            var siteKey = new Key(MASTER_KEY_ID, -1, DateTime.Now, DateTime.Now.AddDays(1), DateTime.Now, MASTER_SECRET);
-            var rawUid = "dGVzdCB1aWQ=";
-            var expectedToken = "expected token";
-
-            string actualRawUid = null;
-            Key actualMasterKey = null;
-            Key actualSiteKey = null;
-            int actualSideId = -100;
-            UID2TokenGenerator.Params actualParams = null;
-
-            string FakeV4Generate(string rawUid, Key masterKey, int sideId, Key siteKey, UID2TokenGenerator.Params encryptParams)
-            {
-                actualRawUid = rawUid;
-                actualMasterKey = masterKey;
-                actualSideId = sideId;
-                actualSiteKey = siteKey;
-                actualParams = encryptParams;
-                return expectedToken;
-            }
-
-            var token = new AdvertisingTokenBuilder(null, null, FakeV4Generate)
-                .WithPrivacyBits(privacyBits)
-                .WithVersion(TokenVersion.V4)
-                .WithExpiry(expiry)
-                .WithMasterKey(masterKey)
-                .WithRawUid(rawUid)
-                .WithSiteKey(siteKey)
-                .Build();
+            private readonly Key _someKey = new(75, 10, DateTime.Now.AddSeconds(42), DateTime.Now.AddHours(42), DateTime.Now.AddDays(42), MASTER_SECRET);
             
-            Assert.Equal(expectedToken, token);
-            Assert.Equal(rawUid, actualRawUid);
-            Assert.Equal(masterKey, actualMasterKey);
-            Assert.Equal(SITE_ID, actualSideId);  // Default, never overridden
-            Assert.Equal(siteKey, actualSiteKey);
-
-            Assert.NotNull(actualParams);
-            Assert.Equal(privacyBits, actualParams.PrivacyBits);
-            Assert.Equal(expiry, actualParams.TokenExpiry);
-            Assert.Equal((int) IdentityScope.UID2, actualParams.IdentityScope); // Default, never overridden
+            [Fact]
+            public void Version()
+            {
+                var builder = Builder().WithVersion(TokenVersion.V3);
+                Assert.Equal(TokenVersion.V3, builder.Version);
+            }
+            
+            [Fact]
+            public void RawUid()
+            {
+                var rawUid = "raw uid";
+                var builder = Builder().WithRawUid(rawUid);
+                Assert.Equal(rawUid, builder.RawUid);
+            }
+            
+            [Fact]
+            public void MasterKey()
+            {
+                var builder = Builder().WithMasterKey(_someKey);
+                Assert.Equal(_someKey, builder.MasterKey);
+            }
+            
+            [Fact]
+            public void SiteKey()
+            {
+                var builder = Builder().WithSiteKey(_someKey);
+                Assert.Equal(_someKey, builder.SiteKey);
+            }
+            
+            [Fact]
+            public void SiteId()
+            {
+                // There is no setter, default only
+                var builder = Builder();
+                Assert.Equal(SITE_ID, builder.SiteId);
+            }
+            
+            [Fact]
+            public void PrivacyBits()
+            {
+                var privacyBits = 42;
+                var builder = Builder().WithPrivacyBits(privacyBits);
+                Assert.Equal(privacyBits, builder.PrivacyBits);
+            }
+            
+            [Fact]
+            public void Expiry()
+            {
+                var expiry = DateTime.Now.AddHours(42);
+                var builder = Builder().WithExpiry(expiry);
+                Assert.Equal(expiry, builder.Expiry);
+            }
+            
+            [Fact]
+            public void Scope()
+            {
+                // There is no setter, default only
+                var builder = Builder();
+                Assert.Equal(IdentityScope.UID2, builder.Scope);
+            }
         }
     }
 }

e04e7550840b051b3761f1f1abb2e5e589771e37
Gian Miguel Del Mundo <gianmiguel.delmundo@thetradedesk.com>
2023-10-09 15:37:25 +0800

    Updated SDK version to 5.2.12

--- src/UID2.Client/UID2Client.cs
+++ src/UID2.Client/UID2Client.cs
@@ -134,7 +134,7 @@ namespace UID2.Client
 
         private string GetAssemblyNameAndVersion()
         {
-            var version = "5.2.11";
+            var version = "5.2.12";
             return "uid-client-net-" + version;
         }
 
